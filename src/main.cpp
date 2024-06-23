1dbb929c65785875c5150eeff53ad0ffb0d35cbd
NathanChase22 <ntc477@utexas.edu>
2024-06-22 23:40:17 -0500

    first attempt at echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 int main() {
   // REPL (read eval print loop)
@@ -15,10 +17,39 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
+    //input stream reads
+    std::istringstream iss(input);
+    //create array list or 'vector' to store tokens
+    std::vector<std::string> tokens;
+    std::string tok;
+
+    //extracts out from stream a token delimited by a space, 
+    //returns input string or null once finished
+    while (std::getline(iss, tok, ' ')) {
+      tokens.push_back(tok);
+    }
+
+    std::string cmd = tokens.at(0);
+    
     //if recognized command then run
-    if (input == "exit 0") {
-      //wonder if there's a way to parse out the exit code rather than it being hardcoded?
-      exit(0);
+    if (cmd == "echo") {
+      //print out output, what's the best way to do this
+      //go back to input and print a substring that disincludes the first token? 
+      //find position of first ' ' delimiter
+      size_t pos = input.find(' ',0);
+      //check to see if the position is within bounds
+      if (pos != std::string::npos) {
+        std::cout << input.substr(pos+1) << std::endl;
+      }
+    }
+    else if (cmd == "exit") {
+      //try to parse parse second token and convert to int
+      try {
+        int ext_code = std::stoi(tokens.at(1));
+        exit(ext_code);
+      } catch (const std::exception& e) {
+        std::cerr << "Invalid argument to exit: " << tokens.at(1) << std::endl;
+      }
     } else { //ERROR 
       std::cout << input << ": command not found" << std::endl;
     }
