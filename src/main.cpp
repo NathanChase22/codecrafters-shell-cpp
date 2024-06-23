11ebd0bc730a03e33f302a49ad820c51caa6150d
NathanChase22 <ntc477@utexas.edu>
2024-06-22 22:47:48 -0500

    1st attempt at REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  // REPL (read eval print loop)
+  // when will there be a exit command other than an interrupt???
+  while (1) { 
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  
-  //create a string variable and use it store console input
-  std::string input;
-  std::getline(std::cin, input);
+    // Uncomment this block to pass the first stage
+    std::cout << "$ ";
+    
+    //create a string variable and use it store console input
+    std::string input;
+    std::getline(std::cin, input);
 
-  // handle unknown command
-  std::cout << input << ": command not found" << std::endl;
+    // handle unknown command
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
